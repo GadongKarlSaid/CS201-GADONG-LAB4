2289850500517babe2ee6379be265ff13d8e5e48
KARL SAID B. GADONG <146708073+GadongKarlSaid@users.noreply.github.com>
2023-10-09 19:14:45 -0700

    feat: Activity no.4 in DSA
    Switch Conditional Statement in JavaScript

---
+++ script.js
@@ -0,0 +1,16 @@
+//Activity no.4
+let fruit = "banana";
+
+switch(fruit){
+    case 'banana':
+        console.log("Banana so good!")
+         break;
+    case 'apple':
+        console.log("How you like them Apples?");
+         break;
+    case 'orange':
+        console.log("Im not a fan of Orange");
+        break;
+    default:
+        console.log("I have never ever heard that fruit");
+}
